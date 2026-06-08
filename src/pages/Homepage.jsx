67347aae52998790698cc59c6655be306101a6f5
[9025481@student.zadkine.nl] <9025481@student.zadkine.nl>
2026-06-08 18:40:40 +0200

    commit

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,6 +4,11 @@ const Homepage = () => {
     return (
         <>
 
+            <div className="hero-container">
+                <img src="/src/assets/react-logo.png" alt="" />
+                <h1>React</h1>
+                <p>Een JavaScript-bibliotheek voor het bouwen van gebruikersinterfaces.</p>
+            </div>
 
         </>
     );
