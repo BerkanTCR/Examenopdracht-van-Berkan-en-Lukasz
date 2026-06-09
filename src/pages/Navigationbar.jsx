d6948b15401c7bd1290363c1425102c10c284dd6
[9025481@student.zadkine.nl] <9025481@student.zadkine.nl>
2026-06-09 19:29:50 +0200

    commit

--- src/pages/Navigationbar.jsx
+++ src/pages/Navigationbar.jsx
@@ -17,11 +17,7 @@ const Navigationbar = () => {
                 </ul>
 
                 <div className="searchbar-container">
-<<<<<<< Updated upstream
                     <button type="submit"><img src="/src/assets/search-icon.png" alt="" /></button>
-=======
-
->>>>>>> Stashed changes
                     <input type="text" placeholder="Zoeken..." />
                 </div>
 
